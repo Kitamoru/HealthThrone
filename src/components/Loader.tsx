ea56cd89f8e7fc50b5731af51303722da9c2e422
Vadim Ivanov <102748462+Kitamoru@users.noreply.github.com>
2025-05-30 15:14:39 +0300

    Update Loader.tsx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,6 +4,7 @@ export const Loader: React.FC = () => {
   return (
     <div className="loader-container">
       <div className="loader" />
+      <div className="loader-spinner">🚀</div>
       <p>Загружаем данные...</p>
     </div>
   );
