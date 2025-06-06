0a53a976d946aeb5e236c47d36e196fbb5d9a68b
Vadim Ivanov <102748462+Kitamoru@users.noreply.github.com>
2025-06-06 23:29:11 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -25,6 +25,16 @@ const nextConfig = {
         ]
       }
     ];
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer/')
+    };
+    
+    return config;
   }
 };
 
