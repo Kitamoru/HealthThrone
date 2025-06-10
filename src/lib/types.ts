2f8c9a2dc821f7fddb35b804c62d10b053d84b1a
Vadim Ivanov <102748462+Kitamoru@users.noreply.github.com>
2025-06-10 10:32:19 +0300

    Update types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -49,7 +49,7 @@ export interface UserProfile {
 export interface Friend {
   id: number;
   created_at: string;
-  last_name?: number;
+  friend_id?: number;
   friend: {
     id: number;
     first_name: string;
