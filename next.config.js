403202335845296e365c6ba825490b706774184d
Vadim Ivanov <102748462+Kitamoru@users.noreply.github.com>
2025-06-16 14:06:37 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -38,4 +42,4 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
