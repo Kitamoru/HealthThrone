df6159c02fa952db9d1aabfcd4763e6c63e676b6
Vadim Ivanov <102748462+Kitamoru@users.noreply.github.com>
2025-06-24 12:01:36 +0300

    Update Loader.tsx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
 export function Loader() {
-  const [bgColor, setBgColor] = useState('#131313'); // Значение по умолчанию
   return (
     <div style={{
       position: 'relative',
