fb1323e2899bcb967f85fe4b67a0262ff0f2b414
Vadim Ivanov <102748462+Kitamoru@users.noreply.github.com>
2025-06-30 21:38:40 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,7 @@ import Onboarding from '../components/Onboarding';
 import Octagram from '../components/Octagram';
 import { SurveyModal } from '../components/SurveyModal';
 import { createPortal } from 'react-dom';
+import '../styles/globals.css';
 
 interface Question {
   id: number;
