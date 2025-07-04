69785351b61b142aa133925a8bfd910637545428
Vadim Ivanov <102748462+Kitamoru@users.noreply.github.com>
2025-07-04 22:51:28 +0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -103,7 +103,7 @@ function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="theme-color" content="#18222d" />
       </Head>
-
+      
       <Script 
         src="https://telegram.org/js/telegram-web-app.js" 
         strategy="beforeInteractive" 
