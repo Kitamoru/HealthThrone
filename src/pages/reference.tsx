e5f81673ea290a2faf19e622f759f755346e975e
Vadim Ivanov <102748462+Kitamoru@users.noreply.github.com>
2025-07-07 18:43:55 +0300

    Update reference.tsx

--- src/pages/reference.tsx
+++ src/pages/reference.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useTelegram } from '../hooks/useTelegram';
 import Head from 'next/head';
+import BottomMenu from '../components/BottomMenu'; // Импортируем компонент меню
 
 const ReferencePage = () => {
   const router = useRouter();
@@ -84,33 +85,8 @@ const ReferencePage = () => {
             </div>
           </div>
         </div>
-
-        {/* Меню навигации */}
-        <div className="menu">
-  <Link href="/" passHref>
-    <button className={`menu-btn ${router.pathname === '/' ? 'active' : ''}`}>
-      <img src="/1.svg" alt="Dashboard" className="menu-btn-icon" />
-    </button>
-  </Link>
-  
-  <Link href="/friends" passHref>
-    <button className={`menu-btn ${router.pathname === '/friends' ? 'active' : ''}`}>
-      <img src="/2.svg" alt="Friends" className="menu-btn-icon" />
-    </button>
-  </Link>
-  
-  <Link href="/shop" passHref>
-    <button className={`menu-btn ${router.pathname === '/shop' ? 'active' : ''}`}>
-      <img src="/3.svg" alt="Shop" className="menu-btn-icon" />
-    </button>
-  </Link>
-  
-  <Link href="/reference" passHref>
-    <button className={`menu-btn ${router.pathname === '/reference' ? 'active' : ''}`}>
-      <img src="/4.svg" alt="Info" className="menu-btn-icon" />
-    </button>
-  </Link>
-</div>
+        
+    <BottomMenu />
       </div>
     </>
   );
