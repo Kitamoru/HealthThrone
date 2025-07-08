61a5f7e3f50935793761396b2a0aef46ce60d376
Vadim Ivanov <102748462+Kitamoru@users.noreply.github.com>
2025-07-08 08:17:24 +0300

    Update BottomMenu.tsx

--- src/components/BottomMenu.tsx
+++ src/components/BottomMenu.tsx
@@ -7,34 +7,34 @@ const BottomMenu = () => {
   const router = useRouter();
 
   return (
-    <div className={styles.menu}>
+    <div className="bottom-menu">
       <Link href="/" passHref legacyBehavior>
-        <div className={`${styles['menu-btn']} ${router.pathname === '/' ? styles.active : ''}`}>
-          <svg className={styles['menu-icon']} width="28" height="28" viewBox="0 0 24 24">
+        <div className={`menu-btn ${router.pathname === '/' ? 'active' : ''}`}>
+          <svg className="menu-icon" width="28" height="28" viewBox="0 0 24 24">
             <use xlinkHref="/1.svg#icon" />
           </svg>
         </div>
       </Link>
       
       <Link href="/friends" passHref legacyBehavior>
-        <div className={`${styles['menu-btn']} ${router.pathname === '/friends' ? styles.active : ''}`}>
-          <svg className={styles['menu-icon']} width="28" height="28" viewBox="0 0 24 24">
+        <div className={`menu-btn ${router.pathname === '/friends' ? 'active' : ''}`}>
+          <svg className="menu-icon" width="28" height="28" viewBox="0 0 24 24">
             <use xlinkHref="/2.svg#icon" />
           </svg>
         </div>
       </Link>
       
       <Link href="/shop" passHref legacyBehavior>
-        <div className={`${styles['menu-btn']} ${router.pathname === '/shop' ? styles.active : ''}`}>
-          <svg className={styles['menu-icon']} width="28" height="28" viewBox="0 0 24 24">
+        <div className={`menu-btn ${router.pathname === '/shop' ? 'active' : ''}`}>
+          <svg className="menu-icon" width="28" height="28" viewBox="0 0 24 24">
             <use xlinkHref="/3.svg#icon" />
           </svg>
         </div>
       </Link>
       
       <Link href="/reference" passHref legacyBehavior>
-        <div className={`${styles['menu-btn']} ${router.pathname === '/reference' ? styles.active : ''}`}>
-          <svg className={styles['menu-icon']} width="28" height="28" viewBox="0 0 24 24">
+        <div className={`menu-btn ${router.pathname === '/reference' ? 'active' : ''}`}>
+          <svg className="menu-icon" width="28" height="28" viewBox="0 0 24 24">
             <use xlinkHref="/4.svg#icon" />
           </svg>
         </div>
